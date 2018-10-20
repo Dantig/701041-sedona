815b8cd220b1f7ddfdd6a175836bc49fabab7fe8
Timur Gumerov <tgumerov@gmail.com>
2018-10-20 22:23:47 +0500

    Добавил маркер на карту, поправил стили для FF и IE11

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,26 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var del = require("del");
+var csso = require("gulp-csso");
+var rename = require("gulp-rename");
+var svgstore = require("gulp-svgstore");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
+var uglify = require("gulp-uglify");
+
+gulp.task("copy", function () {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/*.html"
+  ], { base: "source" })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function () {
+  return del("build");
+});
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -14,18 +34,59 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("source/css"))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
-gulp.task("add-normalize", function () {
-  return gulp.src("source/sass/normalize.css")
-    .pipe(gulp.dest("source/css"))
+gulp.task("sprite", function () {
+  return gulp.src("source/img/sprite/**/*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"))
+});
+
+gulp.task("images", function () {
+  return gulp.src("build/img/**/*.{jpg,png,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("webp", function () {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("source/img/webp"));
+});
+
+gulp.task("refresh", function (done) {
+  server.reload();
+  done();
+});
+
+gulp.task("js", function () {
+  return gulp.src("source/js/**/*.js")
+    .pipe(uglify())
+    .pipe(rename(function (path) {
+      path.basename += ".min";
+    }))
+    .pipe(gulp.dest("build/js"))
+});
+
+gulp.task("copy-html", function () {
+  return gulp.src("source/*.html")
+    .pipe(gulp.dest("build"));
 });
 
 gulp.task("server", function () {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -33,8 +94,10 @@ gulp.task("server", function () {
   });
 
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/img/sprite/*.svg", gulp.series("sprite", "refresh"));
+  gulp.watch("source/*.html", gulp.series("copy-html", "refresh"));
+  gulp.watch("source/js/**/*.html", gulp.series("js", "refresh"));
 });
 
-gulp.task("start", gulp.series("add-normalize", "css", "server"));
-gulp.task("build", gulp.series("add-normalize", "css"));
+gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "js"));
+gulp.task("start", gulp.series("build", "server"));
